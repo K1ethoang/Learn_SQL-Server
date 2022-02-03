9cee2ddeba26fcf700abd0db6bea1878acbcb916
Hoang Gia Kiet <kiethoang101@gmail.com>
2022-02-03 17:07:10 +0700

    Add: image and file

---
+++ 9.1.Inner Join/inner join.sql
@@ -0,0 +1,13 @@
+﻿USE HowKteam
+GO	
+
+-- inner join -> kiểu cũ
+-- mọi join đều đi kèm điều kiện
+SELECT * FROM 
+dbo.GIAOVIEN, dbo.BOMON
+WHERE dbo.GIAOVIEN.MABM = dbo.BOMON.MABM
+
+-- chuẩn
+SELECT * FROM
+dbo.GIAOVIEN INNER JOIN dbo.BOMON
+ON BOMON.MABM = GIAOVIEN.MABM
