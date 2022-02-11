b77477140aca1b023f2e01f6c231cea73f499237
Hoang Gia Kiet <kiethoang101@gmail.com>
2022-02-11 20:27:41 +0700

    Add: lesson 13

---
+++ 13.Auto increament/README.md
@@ -0,0 +1,16 @@
+# 1. Auto Increament
+- Xem code để hiểu rõ hơn
+- [Code](link)
+
+# 2. Syntax
+
+```sql
+CREATE TABLE <tên table>
+(
+	<tên trường> <kiểu dữ liệu> IDENTITY, 
+	-- tự tăng trường này, phải là số
+	-- Mặc định từ 1 và tăng 1 đơn vị
+	<tên trường> <kiểu dữ liệu>
+	...
+)
+```
---
+++ 13.Auto increament/auto increament.sql
@@ -0,0 +1,31 @@
+﻿USE HowKteam
+GO
+
+CREATE TABLE TestAutoIncreament
+(
+	id INT PRIMARY KEY IDENTITY, 
+	-- tự tăng trường này, phải là số
+	-- Mặc định từ 1 và tăng 1 đơn vị
+	name NVARCHAR(100)
+)
+
+-- khi insert không cần trường id
+INSERT INTO dbo.TestAutoIncreament
+(name) VALUES
+(N'Kiệt'),(N'Như'),(N'Nam'),(N'Tuấn')
+
+SELECT * FROM dbo.TestAutoIncreament
+
+-- TRUNCATE TABLE dbo.TestAutoIncreament -- xoá dữ liệu trong bảng (không xoá bảng)
+
+CREATE TABLE TestAutoIncreament2
+(
+	id INT PRIMARY KEY IDENTITY(5,10),
+	-- tự tăng bắt đầu từ 5. mỗi lần tăng 10 đơn vị
+	name NVARCHAR(100)
+)
+
+INSERT INTO dbo.TestAutoIncreament2
+(name) VALUES
+(N'Kiệt'),(N'Như'),(N'Nam'),(N'Tuấn')
+SELECT * FROM dbo.TestAutoIncreament2
