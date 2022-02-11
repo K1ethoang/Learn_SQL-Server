34b98e4198b3b52bb52c6b4297726e92d08f5e30
Hoang Gia Kiet <kiethoang101@gmail.com>
2022-02-11 15:10:47 +0700

    Add: lesson: 12

---
+++ 12.Group by - having/README.md
@@ -0,0 +1,18 @@
+# 1. Group By - Having
+- having như where của select nhưng giành cho group by
+- having là where của group by
+- Xem code để hiểu rõ hơn
+- [Code](link)
+
+|Hình|
+|:---:|
+|![Group By - Having](link)|
+
+# 2. Syntax
+
+```sql
+SELECT <trường trong GROUP BY hoặc Aggregate Function> FROM TableA
+-- WHERE <điều kiện> (có thể có hoặc không)
+GROUP BY <trường cần group>
+HAVING <điều kiện>
+```
---
+++ 12.Group by - having/group by - having.sql
@@ -0,0 +1,18 @@
+﻿USE HowKteam
+GO
+
+-- xuất ra số lượng giáo viên trong từng bộ môn mà số giáo viên > 1
+-- having -> như where của select nhưng giành cho group by
+-- having là where của group by
+SELECT bm.MABM, COUNT(*) FROM dbo.GIAOVIEN gv, dbo.BOMON bm
+WHERE gv.MABM = bm.MABM 
+GROUP BY bm.MABM HAVING COUNT(*) > 1
+
+-- xuất ra mức lương và tổng số tuổi của các giáo viên nhận mức lương đó
+-- và có người thân
+-- và có lương > 2000
+SELECT LUONG, SUM(YEAR(GETDATE()) - YEAR(gv.NGSINH)) FROM dbo.GIAOVIEN gv,dbo.NGUOITHAN nt
+WHERE gv.MAGV = nt.MAGV 
+AND	gv.MAGV IN (SELECT gv.MAGV FROM	dbo.NGUOITHAN)
+GROUP BY LUONG,gv.NGSINH
+HAVING gv.LUONG > 2000
