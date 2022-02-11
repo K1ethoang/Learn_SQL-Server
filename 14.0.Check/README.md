32af0f381246b2a1cefe9e97e6462b102b75de53
Hoang Gia Kiet <kiethoang101@gmail.com>
2022-02-11 21:40:14 +0700

    Add: lesson 14.0

---
+++ 14.0.Check/README.md
@@ -0,0 +1,3 @@
+# Check
+- Xem code để hiểu rõ hơn
+- [Code](https://github.com/K1ethoang/SQL-Server/blob/main/14.0.Check/check.sql)
---
+++ 14.0.Check/check.sql
@@ -0,0 +1,41 @@
+﻿USE HowKteam
+GO
+
+-- cách 1
+CREATE TABLE testCheck
+(
+	id INT CONSTRAINT CK_id PRIMARY KEY IDENTITY CHECK(id <= 10),
+	luong INT CONSTRAINT CK_luong	CHECK (luong > 3000 AND luong < 9000)
+)
+GO 
+
+INSERT dbo.testCheck
+(luong) VALUES
+(3001),(4000),(8999)
+GO
+
+SELECT * FROM dbo.testCheck
+
+/*
+-- cách 2
+CREATE TABLE testCheck2
+(
+	id INT CONSTRAINT CK_id2 PRIMARY KEY IDENTITY CHECK(id <= 10),
+	luong INT,
+	CONSTRAINT CK_luong	CHECK2 (luong > 3000 AND luong < 9000)
+)
+GO
+
+-- cách 3
+CREATE TABLE testCheck3
+(
+	id INT CONSTRAINT CK_id3 PRIMARY KEY IDENTITY CHECK(id <= 10),
+	luong INT
+)
+
+ALTER TABLE dbo.testCheck3
+ADD CONSTRAINT CK_luong3	CHECK (luong > 3000 AND luong < 9000)
+*/
+
+-- xoá bảng
+DROP TABLE dbo.testCheck
