51e57880743ce275f9f372dac22b212f5c2824e2
Hoang Gia Kiet <kiethoang101@gmail.com>
2022-02-12 22:25:23 +0700

    Add: lesson 15.1

---
+++ 15.1. Cursor (T_SQL)/README.md
@@ -0,0 +1,44 @@
+# 1. Cursor (con trỏ)
+- **Tác dụng**: Khi có nhu cầu duyệt từng record của bảng, với mỗi record có kết quả xử lý riêng thì dùng cursor
+- Nếu đã code C/C++ thì qua đây thì tư duy vẫn vậy, chỉ khác cú pháp
+- Bên SQL gọi là Cursor thay vì Pointer 🍦
+- [Code](link)
+  
+|Hình|
+|:---:|
+|![cursor](./image/SQL-Server-Cursor.png)|
+# 2. Syntax
+## Các bước như hình
+## 1. Khai báo con trỏ
+```sql
+DECLARE <tên con trỏ> CURSOR
+FOR <câu lệnh select>
+```
+## 2. Mở con trỏ
+```sql
+OPEN <tên con trỏ>
+```
+
+## 2. Mở con trỏ
+```sql
+FETCH NEXT FROM <tên con trỏ> INTO <danh sách biến>
+```
+
+```sql
+WHILE @@FETCH_STATUS = 0  
+    BEGIN
+        <câu lệnh>
+        FETCH NEXT FROM <tên con trỏ> INTO <danh sách biến>  -- tăng biến con trỏ (chuyển qua phần tử khác)
+    END;
+
+```
+
+## 4. Đóng con trỏ
+```sql
+CLOSE <tên con trỏ>
+```
+
+## 5. Giải phóng
+```sql
+DEALLOCATE <tên con trỏ>
+```
---
+++ 15.1. Cursor (T_SQL)/cursor.sql
@@ -0,0 +1,56 @@
+﻿USE HowKteam
+GO
+
+-- từ tuổi của giáo viên
+-- nếu lớn hơn 60 thì cho lương cộng thêm 3000
+-- Nếu nhỏ hơn 50 và lớn hơn 60 thì cộng thêm 1500
+-- Ngược lại thì lương 500
+
+-- trước tiên cần clone bảng GiaoVien
+SELECT * INTO CloneGV
+FROM dbo.GIAOVIEN
+
+-- code
+---------------------------
+
+-- lấy ra danh sách MaGV và Tuổi đưa vào con trỏ có tên là GiaoVienCursor 
+DECLARE GiaoVienCursor CURSOR
+FOR SELECT MAGV, YEAR(GETDATE()) - YEAR(NGSINH) AS [Tuổi Giáo Viên] FROM dbo.cloneGV
+
+OPEN GiaoVienCursor;
+
+DECLARE @MaGV VARCHAR(10), 
+		@Tuoi INT;
+
+FETCH NEXT FROM GiaoVienCursor INTO	@MaGV,@Tuoi;
+
+WHILE @@FETCH_STATUS = 0
+	BEGIN
+		IF @Tuoi > 60
+			BEGIN
+			    UPDATE dbo.cloneGV SET LUONG = 3000 WHERE MAGV = @MaGV;
+			END
+		ELSE
+			BEGIN
+				IF @Tuoi > 50 AND @Tuoi < 60
+					BEGIN
+						UPDATE dbo.cloneGV SET LUONG = 1500 WHERE MAGV = @MaGV;
+					END
+				ELSE
+					BEGIN
+						UPDATE dbo.cloneGV SET LUONG = 500 WHERE MAGV = @MaGV;		    
+					END
+			END
+		FETCH NEXT FROM GiaoVienCursor INTO @MaGV, @Tuoi;
+	END 
+
+CLOSE GiaoVienCursor;
+DEALLOCATE GiaoVienCursor;
+
+------------------------------
+
+-- xem CloneGV
+SELECT * FROM dbo.cloneGV
+
+-- xoá clone GV
+DROP TABLE dbo.cloneGV
