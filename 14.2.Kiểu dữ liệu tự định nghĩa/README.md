e01294689bda0fee258b603d0337c514a616b3e8
Hoang Gia Kiet <kiethoang101@gmail.com>
2022-02-12 15:23:47 +0700

    Add: lesson 14.2

---
+++ 14.2.Kiểu dữ liệu tự định nghĩa/README.md
@@ -0,0 +1,9 @@
+# 1. Kiểu dữ liệu tự định nghĩa
+- Xem code để hiểu rõ hơn
+- [Code](link)
+
+# 2. Syntax
+```sql
+EXEC sys.sp_addtype '<tên kiểu dữ liệu>', '<kiểu dữ liệu thực tế', 'Not null' -- (có hay không đều được)
+```
+
---
+++ 14.2.Kiểu dữ liệu tự định nghĩa/kiểu dữ liệu tự định nghĩa.sql
@@ -0,0 +1,27 @@
+﻿USE HowKteam
+GO
+
+-- kiểu dữ liệu tự định nghĩa
+-- 2 câu dưới có ý nghĩa như nhau
+EXEC sys.sp_addtype 'NName', 'nvarchar(100)','not null'   
+EXEC sp_addtype 'NName2', 'nvarchar(100)', 'Not null'
+
+CREATE TABLE testType
+(
+	name NName,
+	address NVARCHAR(100)
+)
+GO
+
+
+--xoá bảng
+DROP TABLE dbo.testType
+
+-- xoá type vừa định nghĩa
+-- nếu có bảng sử dụng kiểu dữ liệu trên thì phải xoá bảng trước
+
+-- cách 1
+DROP TYPE NName
+-- cách 2
+EXEC sys.sp_droptype NName
+EXEC sp_droptype NName2
